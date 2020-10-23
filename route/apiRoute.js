133bd3f5790d6d2761c758f224b635a8d7c08b28
Manii-Dot <emmanuel.stephen68@gmail.com>
2020-10-23 09:49:26 -0500

    added s to notes route on both the api and html route file.

--- route/apiRoute.js
+++ route/apiRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const store = require("../db/store.js");
 const router = require("express").Router();
 
-router.get("/note", (req,res)=>{
+router.get("/notes", (req,res)=>{
     store.getNote().then((note) => res.json(note))
 })
 
